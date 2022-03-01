f476c66571a7f54963c7b2dc944a95a89975ef9c
Prashant Srivastava <prashantsrivastava381@gmail.com>
2022-03-02 00:20:35 +0530

    CRUD Done

--- app.js
+++ app.js
@@ -35,6 +35,58 @@ app.post('/api/v1/create',async(req,res)=>{
     })
 })
 
+//Read data
+app.post('/api/v1/read',async(req,res)=>{
+    //find product
+    const product=await Product.find();
+
+    res.status(200).json({
+        success:true,
+        product
+    })
+})
+
+//Update
+app.put('/api/v1/update/:id',async(req,res)=>{
+
+    //check if this user exist or not
+    let product=await Product.findById(req.params.id);
+    //if user not exist
+    if(!product){
+       return res.status(500).json({
+            success:false,
+            message:"User Dont exits with this id"
+        })
+    }
+    //if user exist then update
+    product=await Product.findByIdAndUpdate(req.params.id,req.body,{new:true,
+        useFindAndModify:true,
+        runValidators:true
+    });
+    res.status(201).json({
+        success:true,
+        product
+    })
+})
+
+//Delete
+app.delete('/api/v1/delete/:id',async(req,res)=>{
+    const product=await Product.findById(req.params.id);
+    if(!product){
+       return res.status(500).json({
+            success:false,
+            message:"User with this Id dont exist"
+        })
+    }
+    
+    //if we find product
+    product.remove();
+    res.status(200).json({
+        success:true,
+        message:"Successfully Deleted"
+    })
+})
+
 app.listen(3000, () => {
   console.log(`Example app listening on port 3000`)
 })
